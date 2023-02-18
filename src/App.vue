5372f7492553e329b75331dc8223cf0ba93e1b16
Ben.Chen <ben.chen@keyxentic.com>
2023-02-18 14:18:25 +0800

    refactor: 移除初始歡迎畫面

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" :class="' border-red-100 border-2'" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template lang="pug">
+div
+  a(href="https://vitejs.dev" target="_blank")
+    img(src="/vite.svg")
+    p {{ test.name  }}
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+interface test {
+  name: string
+}
+
+const test = ref<test>({
+  name: "gogogo"
+})
+</script>
 
 <style scoped>
 .logo {
@@ -21,9 +24,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
