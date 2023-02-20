d0805880c117a6c451bf43d777ab10c0fa559d9b
Ben.chen <ben.chen@keyxentic.com>
2023-02-20 22:22:15 +0800

    feat: 新增 `TreeList.vue` 遞迴渲染組件

---
+++ apps/Nested-tree/src/components/TreeList.vue
@@ -0,0 +1,25 @@
+<template lang='pug'>
+div(class="tree")
+    div(v-for="(item, index) in data" :key="index")
+        div.cursor-pointer(@click="toggleSwitchControl(item)")
+            span.font-bold {{ item.name }}
+            span.ml-1(v-if='item.child?.length !== 0') {{ item.isOpen? '[+]' : '[-]' }}
+            span.ml-1(v-else-if='item.name !== ""') {{ ":" }}
+            span(v-if="item.value!== null && item.name !== ''").ml-3.text-rose-300 {{ item.value }}
+        TreeList(v-if="item.child?.length !== 0 && item.isOpen" :data='item.child').pl-8
+</template>
+<script setup lang="ts">
+import TreeList from './TreeList.vue'
+import type { TreeStructure } from '@/types/index.mjs';
+
+type Props = {
+    data?: TreeStructure[]
+}
+
+defineProps<Props>();
+
+function toggleSwitchControl(item: TreeStructure) {
+    item.isOpen = !item.isOpen
+}
+</script>
+<style scoped></style>
