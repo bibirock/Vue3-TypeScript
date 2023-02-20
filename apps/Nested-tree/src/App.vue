f6ca171c823a0de9cca52fba6061a7eed1a99c4d
Ben.Chen <ben.chen@keyxentic.com>
2023-02-20 14:02:49 +0800

    feat: 將NestedTreeDemoPage進行掛載到`App.vue`

--- apps/Nested-tree/src/App.vue
+++ apps/Nested-tree/src/App.vue
@@ -1,35 +1,11 @@
 
 <template lang="pug">
-div
-  a(href="https://vitejs.dev" target="_blank")
-    img(src="/vite.svg")
-    p {{ test.name  }}
+div#page-content
+  NestedTreeDemoPage
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-interface test {
-  name: string
-}
-
-const test = ref<test>({
-  name: "gogogo"
-})
+import NestedTreeDemoPage from './components/page/NestedTreeDemoPage.vue';
 </script>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
