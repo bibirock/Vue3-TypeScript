6d7082ac1dfa984e6be986f4bc3c5d846531a0d3
Ben.chen <ben.chen@keyxentic.com>
2023-02-20 23:35:16 +0800

    feat: 新增`Nav.vue`、`UserWall`，並掛載

---
+++ apps/Random_User/src/components/layout/NavBar.vue
@@ -0,0 +1,9 @@
+<template lang='pug'>
+div
+    div(:class="''")
+
+</template>
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+</script>
+<style scoped></style>
---
+++ apps/Random_User/src/components/page/UserWall.vue
@@ -0,0 +1,10 @@
+<template lang='pug'>
+div
+    NavBar
+
+</template>
+
+<script setup lang="ts">
+import NavBar from '@/components/layout/NavBar.vue'
+</script>
+<style scoped></style>
