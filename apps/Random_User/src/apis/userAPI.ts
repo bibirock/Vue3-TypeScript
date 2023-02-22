68233f405b4a41bb51776e64aae8c27ee9d1e9e6
Ben.Chen <ben.chen@keyxentic.com>
2023-02-22 13:55:17 +0800

    feat: `userAPI.ts` 分流請求資料

--- apps/Random_User/src/apis/userAPI.ts
+++ apps/Random_User/src/apis/userAPI.ts
@@ -1,6 +1,7 @@
-const BASEURL = 'https://randomuser.me/api/?results=30';
+import type { RequireUserDataParams } from '@/types/type';
+const BASEURL = 'https://randomuser.me/api/';
 
-export async function $fecthUserData() {
-    const res = await fetch(BASEURL);
+export async function $fecthUserData(params: RequireUserDataParams) {
+    const res = await fetch(`${BASEURL}?page=${params.page}&results=${params.results}&seed=TEST`);
     return res.json();
 }
