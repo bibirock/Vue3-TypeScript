1e7370191acdfb5fc207597d8bc84b8df6ed9736
Ben.Chen <ben.chen@keyxentic.com>
2023-02-22 17:00:48 +0800

    feat: 更新組件名稱`UserWallPage`

--- apps/Random_User/src/App.vue
+++ apps/Random_User/src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import UserWall from '@/components/page/UserWall.vue'
+import UserWall from '@/components/page/UserWallPage.vue'
 </script>
 
 <style scoped></style>
