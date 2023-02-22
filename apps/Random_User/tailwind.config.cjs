9a5ad6f2f05e7a9005f9bb9ab9bcf0ce477099c3
Ben.chen <ben.chen@keyxentic.com>
2023-02-22 22:33:46 +0800

    style: 新增自訂義 style

--- apps/Random_User/tailwind.config.cjs
+++ apps/Random_User/tailwind.config.cjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            dropShadow: {
+                md: '0 10px 10px rgb(183, 195, 226)'
+            }
+        }
+    },
+    plugins: []
+};
