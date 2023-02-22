92f94df05a6dd8f6c79594bc8b45e02231f35608
Ben.chen <ben.chen@keyxentic.com>
2023-02-22 23:28:46 +0800

    style: 將 UserCard 整理成樣式組件

--- apps/Random_User/src/components/layout/UserCard.vue
+++ apps/Random_User/src/components/layout/UserCard.vue
@@ -1,18 +1,48 @@
 <template lang='pug'>
-div.card-list(:class="'p-11'")
-    .user-area(:class="'mx-auto w-11/12 flex flex-wrap flex-row justify-start'")
-        .file(v-for='(user,i) in data' @click="" :class="'m-2'")
-            .img-area(:class="'mx-auto border border-sky-500 max-w-[136px]'")
-                //- img(:class="'p-1 '" :src="user?.picture.large" referrerPolicy="no-referrer")
-                span(:class="'text-center block'") {{ user.name.first }}
+div.card-list(:class="'p-11 mx-auto'")
+    .user-area(:class="'mx-auto w-11/12 set-item-start flex-wrap flex-row'")
+        .user-card(v-for='(user,i) in data' @click="")
+            div.user-card-items
+                img(:src="user?.picture.large" referrerPolicy="no-referrer")
+                div.user-card-info
+                    span.user-name {{ user.name.first }}
+                    Icon.favorite-icon(v-if="checkFavoriteUser(user.login.uuid)" @click="removeFavorite(user.login.uuid)" icon="mdi:heart" color="red" width="25" height="25")
+                    Icon.favorite-icon(v-else @click="addFavorite(user.login.uuid)" icon="mdi:cards-heart-outline" color="white" width="25" height="25")
 </template>
 
 <script setup lang="ts">
-import type { UserDataArr } from '@/types/type'
+import { onMounted, ref, computed } from 'vue';
+import type { UserDataArr, UserData, } from '@/types/type'
+import { Icon } from '@iconify/vue';
+
+type UserId = UserData['login']['uuid']
+
 type Props = {
     data?: UserDataArr
 }
+
+
 defineProps<Props>()
 
+onMounted(() => {
+    getFavoriteList()
+})
+
+const favorite = ref<Array<UserId>>()
+function getFavoriteList() {
+    const localFavorite = JSON.parse(window.sessionStorage.getItem('favorite') || '{}')
+    if (localFavorite !== null) {
+        favorite.value = localFavorite
+    }
+}
+
+function addFavorite(id: UserId) {
+    favorite.value?.push(id)
+    window.sessionStorage.setItem('favorite', JSON.stringify(favorite.value))
+}
+
+function checkFavoriteUser(id: UserId) {
+    return favorite.value?.includes(id)
+}
 </script>
 <style scoped></style>
