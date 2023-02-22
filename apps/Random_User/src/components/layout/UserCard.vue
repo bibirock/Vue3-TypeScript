4f91d4195f86240e4eeec6a70aaf0da978e6ca1c
Ben.chen <ben.chen@keyxentic.com>
2023-02-22 23:34:21 +0800

    feat: `UserCard.vue` 調整class、完成移除功能

--- apps/Random_User/src/components/layout/UserCard.vue
+++ apps/Random_User/src/components/layout/UserCard.vue
@@ -2,16 +2,16 @@
 div.card-list(:class="'p-11 mx-auto'")
     .user-area(:class="'mx-auto w-11/12 set-item-start flex-wrap flex-row'")
         .user-card(v-for='(user,i) in data' @click="")
-            div.user-card-items
+            .user-card-items
                 img(:src="user?.picture.large" referrerPolicy="no-referrer")
-                div.user-card-info
+                .user-card-info
                     span.user-name {{ user.name.first }}
                     Icon.favorite-icon(v-if="checkFavoriteUser(user.login.uuid)" @click="removeFavorite(user.login.uuid)" icon="mdi:heart" color="red" width="25" height="25")
                     Icon.favorite-icon(v-else @click="addFavorite(user.login.uuid)" icon="mdi:cards-heart-outline" color="white" width="25" height="25")
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, computed } from 'vue';
+import { onMounted, ref } from 'vue';
 import type { UserDataArr, UserData, } from '@/types/type'
 import { Icon } from '@iconify/vue';
 
@@ -20,8 +20,6 @@ type UserId = UserData['login']['uuid']
 type Props = {
     data?: UserDataArr
 }
-
-
 defineProps<Props>()
 
 onMounted(() => {
@@ -44,5 +42,11 @@ function addFavorite(id: UserId) {
 function checkFavoriteUser(id: UserId) {
     return favorite.value?.includes(id)
 }
+
+function removeFavorite(id: UserId) {
+    const index = favorite.value?.findIndex((e) => e === id)
+    favorite.value?.splice(index as number, 1)
+    window.sessionStorage.setItem('favorite', JSON.stringify(favorite.value))
+} 
 </script>
 <style scoped></style>
