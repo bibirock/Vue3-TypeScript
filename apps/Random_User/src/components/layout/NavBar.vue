71e66d8da2eaf08d1e0fc74173893291b9592d11
Ben.Chen <ben.chen@keyxentic.com>
2023-02-22 15:50:40 +0800

    feat: `NavBar.vue` 將設定的直傳遞出去

--- apps/Random_User/src/components/layout/NavBar.vue
+++ apps/Random_User/src/components/layout/NavBar.vue
@@ -2,30 +2,43 @@
 div.h-16.flex.justify-between
     div(:class="'flex basis-1/2'")
         div(:class="'w-1/4 flex justify-around items-center ml-5'")
-            span(:class="'cursor-pointer'") {{ "ALL" }}
-            span(:class="'cursor-pointer'") {{ "Favorite" }}
+            span(:class="'cursor-pointer'" @click="bookMark = 'ALL'") {{ "ALL" }}
+            span(:class="'cursor-pointer'" @click="bookMark = 'Favorite'") {{ "Favorite" }}
     div(:class="'flex justify-end items-center basis-1/2'")
         div.mr-5
             a-space(:class="'m-3'")
-                a-select(ref="select" v-model:value="selected" style="width: 120px" @change="getData()")
+                a-select(ref="select" v-model:value="selected" style="width: 120px")
                     a-select-option(v-for="(option, i) in options" :key="i" :value="option.value") {{ option.text }}
             a-switch(v-model:checked="pageMode" checked-children="List" un-checked-children="Card")
 </template>
 <script setup lang="ts">
-import { ref, onMounted, reactive, computed } from 'vue'
+import { ref, reactive, watchEffect } from 'vue'
+import type { SettingData } from '@/types/type'
+const emit = defineEmits(['setPageSettingData']);
 
 const options = reactive([
-    { text: 10, value: 'A' },
-    { text: 20, value: 'B' },
-    { text: 30, value: 'C' }
+    { text: 10, value: 10 },
+    { text: 20, value: 20 },
+    { text: 30, value: 30 }
 ])
 
-const pageMode = ref(true)
-
-const selected = ref("A")
-
-function getData() {
-    console.log(selected.value);
+function emitPageSettingData() {
+    emit('setPageSettingData', pageSettingData)
 }
+
+const bookMark = ref('ALL')
+const selected = ref(30)
+const pageMode = ref(true)
+const pageSettingData: SettingData = reactive({
+    bookMark: bookMark,
+    userCount: selected,
+    dispalyMode: pageMode
+})
+
+watchEffect(() => {
+    if (pageSettingData) {
+        emitPageSettingData()
+    }
+})
 </script>
 <style scoped></style>
