7b36087f752bca975473c1f9e34855f25ae39659
Ben.chen <ben.chen@keyxentic.com>
2023-02-25 13:49:40 +0800

    feat: `UserDetailModel.vue` 功能完成

--- apps/Random_User/src/components/modal/UserDetailModal.vue
+++ apps/Random_User/src/components/modal/UserDetailModal.vue
@@ -1,13 +1,27 @@
 <template lang="pug">
 teleport(to="body")
-    .modal-container(:class="'set-item-center z-10 center'")
-        .modal-mask(@click='closeModal()' :class="'center bg-black/[.5]'")
-        .modal-content(:class="'bg-white z-10 p-5'")  {{ "dddd" }}
-            slot
+    div.modal-container(:class="'set-item-center z-10 fixed-center'")
+        .modal-mask(@click='closeModal()' :class="'fixed-center bg-black/[.5]'")
+        .modal-content(:class="'bg-white z-10 p-10 rounded-xl relative'")
+            Icon(@click='closeModal()' icon="iconoir:cancel" width="25" :class="'absolute top-5 right-5 cursor-pointer'")
+            p.text-center.text-lg {{ "Profile" }}
+            img(:src="user?.picture.large" :class="'block mx-auto rounded-xl border-1'")
+            .info-area(:class="'set-item-center flex-col w-fit mx-auto mt-4'")
+                span {{ "Name : " + user?.name.first }}
+                span {{ "Gender : " + user?.gender }}
+                span {{ "Country : " + user?.location.country }}
+                span {{ "Email : " + user?.email }}
 </template>
 
 <script lang="ts" setup>
+import type { UserData } from '@/types/type';
 const emit = defineEmits(['closeModal']);
+
+type Props = {
+    user?: UserData;
+};
+
+defineProps<Props>();
 
 function closeModal() {
     emit('closeModal');
@@ -15,7 +29,10 @@ function closeModal() {
 </script>
 
 <style>
-.center {
+.fixed-center {
     @apply top-0 bottom-0 left-0 right-0 fixed;
+}
+span {
+    @apply self-start;
 }
 </style>
