c49769f6fbebd3f763e9b49f1bd7f1cfe020047a
Ben.chen <ben.chen@keyxentic.com>
2023-02-26 22:14:45 +0800

    chore: 檔案結構更動

---
+++ apps/Random-user/src/components/layout/LoadingView.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+.text-center.bg-slate-100.rounded.h-full.set-item-center
+    a-spin(size="large")
+</template>
+<script setup></script>
+<style scoped></style>
--- apps/Random-user/src/components/page/userWallPage/UserWallPage.vue
+++ apps/Random-user/src/components/page/userWallPage/UserWallPage.vue
@@ -3,7 +3,8 @@ div(:class="'relative select-none'")
     div(:class="'sticky top-0 bg-white z-10'")
         nav-bar
     div(:class="'h-[600px] overflow-y-auto'")
-        NoDatas(v-if="userData?.length === 0")
+        loading-view(v-if="isLoading")
+        no-datas(v-else-if="userData?.length === 0")
         keep-alive(v-else)
             component(:is="current.views" :key="current.views" :data="userData" @sendUseData="getSelectUserAndOpenModal")
     div(:class="'set-item-center mt-5 pb-5'")
@@ -16,15 +17,17 @@ import type { RequireUserDataParams, UserDataArr, DisplayMode, UserData } from '
 import { $storeSelectedCount, $storePageMode, $getFavoriteList } from '@/lib/userWallPageUtils';
 import { ref, computed, reactive, markRaw, watch, onMounted } from 'vue';
 import UserDetailModal from '@/components/modal/UserDetailModal.vue';
+import UserCard from '@/components/page/userWallPage/element/UserCard.vue';
+import UserList from '@/components/page/userWallPage/element/UserList.vue';
 import NoDatas from '@/components/layout/NoDatas.vue';
-import UserCard from '@/components/layout/UserCard.vue';
-import UserList from '@/components/layout/UserList.vue';
+import LoadingView from '@/components/layout/LoadingView.vue';
 import NavBar from '@/components/layout/NavBar.vue';
 import { useRoute, useRouter } from 'vue-router';
 import { $fecthUserData } from '@/apis/userAPI';
 
 const route = useRoute();
 const router = useRouter();
+const isLoading = ref(false);
 
 const selectUser = ref<UserData>();
 const isShowModal = ref(false);
@@ -34,6 +37,7 @@ function getSelectUserAndOpenModal(userData: UserData) {
 }
 
 onMounted(() => {
+    setLoadState(true);
     getCurrentPageNumber();
     setPageData();
     $getFavoriteList();
@@ -52,13 +56,19 @@ function resetCurrentPage() {
     currentPage.value = 1;
 }
 
+function setLoadState(isShow: boolean) {
+    isLoading.value = isShow;
+}
+
 async function setPageData() {
     if (route.name === 'favorite-page') {
         userData.value = favoriteCurrentPageData();
         if (userData.value.length === 0) resetCurrentPage();
+        setLoadState(false);
         return;
     } else {
         userData.value = await getUserData($storeSelectedCount.value, currentPage.value);
+        setLoadState(false);
     }
 }
 
@@ -106,7 +116,8 @@ function favoriteCurrentPageData() {
 
 watch(
     () => [$storeSelectedCount.value, currentPage.value],
-    () => {
+    async () => {
+        setLoadState(true);
         router.push({ name: route.name as string, query: { page: currentPage.value } });
         setPageData();
     }
