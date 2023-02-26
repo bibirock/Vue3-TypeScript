5607dbf4d7205c44322c1ed13c24ab3f09b233ca
Ben.chen <ben.chen@keyxentic.com>
2023-02-26 21:27:34 +0800

    feat: 新增`NoData.vue`

---
+++ apps/Random_User/src/components/layout/NoDatas.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+a-result(status='404' title='NoDtats' sub-title='Please add favorite user : )')
+  template(#extra)
+</template>
+<script setup></script>
+<style scoped></style>
