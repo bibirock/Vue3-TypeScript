a1d899ee1369c743c0ee2857047bfdc5d4e1e0bb
Ben.chen <ben.chen@keyxentic.com>
2023-03-11 21:08:37 +0800

    feat: 完成`PaginationNav.vue` 頁碼切換功能

--- apps/Random-user/src/components/layout/PaginationNav.vue
+++ apps/Random-user/src/components/layout/PaginationNav.vue
@@ -1,18 +1,29 @@
 <template lang="pug">
 nav
-    ul(:class="'set-item-center'")
-        li
-            a.control-btn(href="#" @click="toPrevPage()" :class="{ 'cursor-not-allowed': current === 1 }")
+    div(:class="'set-item-center'")
+        div(class="set-item-center")
+            button.control-btn(href="#" @click="toPrevPage()" :class="{ 'cursor-not-allowed': current === 1 }")
                 Icon(icon="material-symbols:chevron-left" width="22" height="22")
-        li(v-for="number in totalPages")
-            a.list-item(@click="getSelectPageNumber(number)" :class="getPageItemClass(number)" href="#"  v-show="shouldDisplay(number)") {{ number }}
-        li
-            a.control-btn(href="#" @click="toNextPage()" :class="{ 'cursor-not-allowed': current === totalPages }")
+            button(@click="toPrevPage(jumpPageCount)" :class="'w-6 mx-auto'" v-show="current > 3" @mouseover="isShowRewind = true" @mouseleave="isShowRewind = false")
+                Icon(:class="'ml-1 text-cyan-500'" v-show="isShowRewind" icon="material-symbols:keyboard-double-arrow-left" width="20" height="20")
+                Icon(v-show="!isShowRewind" icon="ion:ellipsis-horizontal" width="20" height="20" :class="'ml-1 text-gray-400 cursor-pointer'")
+        ul(:class="'flex m-0'")
+            li(v-for="number in totalPages")
+                a.list-item(@click="getSelectPageNumber(number)" :class="getPageItemClass(number)" href="#" v-show="shouldDisplay(number) ") {{ number }}
+        div(class="set-item-center")
+            button(@click="toNextPage(jumpPageCount)" :class="'w-6 mx-auto'" v-show="totalPages - current > 3" @mouseover="isShowFastForward = true" @mouseleave="isShowFastForward = false")
+                Icon(:class="'mr-1 text-cyan-500'" v-show="isShowFastForward" icon="material-symbols:keyboard-double-arrow-right" width="20" height="20")
+                Icon(icon="ion:ellipsis-horizontal" v-show="!isShowFastForward" width="20" height="20" :class="'mr-1 text-gray-400 cursor-pointer'")
+            button.control-btn(href="#" @click="toNextPage()" :class="{ 'cursor-not-allowed': current === totalPages }")
                 Icon(icon="material-symbols:chevron-right" width="22" height="22")
 </template>
 
 <script setup lang="ts">
-import { computed, watch } from 'vue';
+import { computed, watch, ref } from 'vue';
+
+const isShowFastForward = ref(false);
+const isShowRewind = ref(false);
+const jumpPageCount = ref(5);
 
 const props = defineProps({
     total: { type: Number, default: 0 },
@@ -24,24 +35,30 @@ const totalPages = computed(() => {
     return Math.ceil(props.total / props.pageSize);
 });
 
-function toPrevPage() {
-    handlePageChange(props.current - 1);
+function toPrevPage(count = 1) {
+    handlePageChange(props.current - count);
 }
 
-function toNextPage() {
-    handlePageChange(props.current + 1);
+function toNextPage(count = 1) {
+    handlePageChange(props.current + count);
 }
 
 function getSelectPageNumber(number: number) {
     handlePageChange(number);
 }
 
-const emit = defineEmits(['page-change']);
+const emit = defineEmits(['pageChange']);
 function handlePageChange(newPage: number) {
-    if (newPage === props.current || newPage < 1 || newPage > totalPages.value) {
+    if (newPage === props.current) {
         return;
     }
-    emit('page-change', newPage);
+    if (newPage < 1) {
+        newPage = 1;
+    }
+    if (newPage > totalPages.value) {
+        newPage = totalPages.value;
+    }
+    emit('pageChange', newPage);
 }
 
 function getPageItemClass(number: number) {
@@ -56,11 +73,12 @@ function shouldDisplay(number: number) {
     const total = totalPages.value;
 
     if (current > 1 && current !== total) {
-        return [current - 1, current, current + 1].includes(number);
-    } else if (props.current === 1) {
-        return [current, current + 1, current + 2].includes(number);
+        return [1, current - 1, current, current + 1, total].includes(number);
+    }
+    if (props.current === 1) {
+        return [current, current + 1, current + 2, , total].includes(number);
     } else {
-        return [total, total - 1, total - 2].includes(number);
+        return [1, total, total - 1, total - 2].includes(number);
     }
 }
 
@@ -76,7 +94,7 @@ watch(
 a.list-item {
     @apply px-3 py-1 mx-1  bg-white border  hover:text-cyan-500 hover:border-cyan-500;
 }
-a.control-btn {
+button.control-btn {
     @apply block px-2 py-1 text-gray-500 bg-white border border-gray-300;
 }
 </style>
