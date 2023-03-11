806b8823e5c55f05ce709447566e96bef72f062a
Ben.chen <ben.chen@keyxentic.com>
2023-03-11 11:10:15 +0800

    feat: `PaginationNav.vue` 完成換頁邏輯，及變更顯示總數時歸零

--- apps/Random-user/src/components/layout/PaginationNav.vue
+++ apps/Random-user/src/components/layout/PaginationNav.vue
@@ -2,25 +2,81 @@
 nav
     ul(:class="'set-item-center'")
         li
-            a.control-btn(href="#" :class="''")
+            a.control-btn(href="#" @click="toPrevPage()" :class="{ 'cursor-not-allowed': current === 1 }")
                 Icon(icon="material-symbols:chevron-left" width="22" height="22")
-        li(v-for="number in pageCount")
-            a.list-item(href="#") {{ number }}
+        li(v-for="number in totalPages")
+            a.list-item(@click="getSelectPageNumber(number)" :class="getPageItemClass(number)" href="#"  v-show="shouldDisplay(number)") {{ number }}
         li
-            a.control-btn(href="#")
-                Icon(icon="material-symbols:chevron-right" width="22" height="22" )
+            a.control-btn(href="#" @click="toNextPage()" :class="{ 'cursor-not-allowed': current === totalPages }")
+                Icon(icon="material-symbols:chevron-right" width="22" height="22")
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, watch } from 'vue';
 
-const pageCount = ref([1, 2, 3]);
+const props = defineProps({
+    total: { type: Number, default: 0 },
+    current: { type: Number, default: 1 },
+    pageSize: { type: Number, default: 30 }
+});
+
+const totalPages = computed(() => {
+    return Math.ceil(props.total / props.pageSize);
+});
+
+function toPrevPage() {
+    handlePageChange(props.current - 1);
+}
+
+function toNextPage() {
+    handlePageChange(props.current + 1);
+}
+
+function getSelectPageNumber(number: number) {
+    handlePageChange(number);
+}
+
+const emit = defineEmits(['page-change']);
+function handlePageChange(newPage: number) {
+    if (newPage === props.current || newPage < 1 || newPage > totalPages.value) {
+        return;
+    }
+    emit('page-change', newPage);
+}
+
+function getPageItemClass(number: number) {
+    return {
+        'text-cyan-500 border-cyan-500': props.current === number,
+        'border-gray-300 text-gray-500': props.current !== number
+    };
+}
+
+function shouldDisplay(number: number) {
+    const current = props.current;
+    const total = totalPages.value;
+
+    if (current > 1 && current !== total) {
+        return [current - 1, current, current + 1].includes(number);
+    } else if (props.current === 1) {
+        return [current, current + 1, current + 2].includes(number);
+    } else {
+        return [total, total - 1, total - 2].includes(number);
+    }
+}
+
+watch(
+    () => props.pageSize,
+    () => {
+        handlePageChange(1);
+    }
+);
 </script>
+
 <style scoped lang="scss">
 a.list-item {
-    @apply px-3 py-1 mx-1 text-gray-500 bg-white border border-gray-300 hover:text-cyan-500 hover:border-cyan-500;
+    @apply px-3 py-1 mx-1  bg-white border  hover:text-cyan-500 hover:border-cyan-500;
 }
 a.control-btn {
-    @apply block px-2 py-1 text-gray-500 bg-white border border-gray-300 hover:text-cyan-500 hover:border-cyan-500;
+    @apply block px-2 py-1 text-gray-500 bg-white border border-gray-300;
 }
 </style>
