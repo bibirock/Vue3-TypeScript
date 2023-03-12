3e4d4d4803894cca2100798516e4e9e55ea77bd8
Ben.chen <ben.chen@keyxentic.com>
2023-03-12 23:04:44 +0800

    feat: 替換`dropdownMenu` 元件

--- apps/Random-user/src/components/layout/NavBar.vue
+++ apps/Random-user/src/components/layout/NavBar.vue
@@ -6,23 +6,47 @@ div.h-16.flex.justify-between
             router-link.text-black(:to="{ name: 'favorite-page' }" active-class="text-cyan-500") {{ "Favorite" }}
     div(:class="'set-item-end '")
         div.set-item-center.mr-5
-            a-space(:class="'m-3'")
-                a-select(ref="select" v-model:value="$storeSelectedCount" style="width: 120px")
-                    a-select-option(v-for="(option, i) in options" :key="i" :value="option.value") {{ option.text }}
+            dropdown-menu(:options="options" :defaultSelected="defaultSelected" @selected-change="changeSelectCount")
             div(:class="'set-item-around w-20'")
                 Icon.inline-block.cursor-pointer(:class="{ 'text-cyan-500': $storePageMode === 'Card' }" @click="$store.updateDisplayMode('Card')" icon="material-symbols:grid-view-rounded" width="24" height="24" )
                 Icon.inline-block.cursor-pointer(:class="{ 'text-cyan-500': $storePageMode === 'List' }" @click="$store.updateDisplayMode('List')" icon="material-symbols:format-list-bulleted-sharp" width="24" height="24" )
 </template>
 <script setup lang="ts">
+import DropdownMenu from './element/DropdownMenu.vue';
 import { $storeSelectedCount, $storePageMode } from '@/lib/userWallPageUtils';
-import { reactive } from 'vue';
+import { reactive, onMounted, nextTick, watchEffect, ref } from 'vue';
 import { userWallSetting } from '@/store';
 const $store = userWallSetting();
 
+interface Option {
+    text: string;
+    value: number;
+}
+
+onMounted(() => {
+    setDefaultSelected();
+});
+
 const options = reactive([
-    { text: 10, value: 10 },
-    { text: 30, value: 30 },
-    { text: 50, value: 50 }
+    { text: '10', value: 10 },
+    { text: '30', value: 30 },
+    { text: '50', value: 50 }
 ]);
+
+const defaultSelected = ref(options.find((e) => e.value === $store.userCount) as Option);
+
+watchEffect(async () => {
+    const selectedOption = await setDefaultSelected();
+    defaultSelected.value = selectedOption as Option;
+});
+
+async function setDefaultSelected() {
+    await nextTick();
+    return options.find((e) => e.value == $storeSelectedCount.value);
+}
+
+function changeSelectCount(value: number) {
+    $storeSelectedCount.value = value;
+}
 </script>
 <style scoped></style>
