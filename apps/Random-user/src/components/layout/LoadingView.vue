c8cd21db3d12a14fcbbb3578103cb463e4485e60
Ben.Chen <ben.chen@keyxentic.com>
2023-03-13 13:56:55 +0800

    refactor: `LoadingView.vue` 使用Icon代替套件內容

--- apps/Random-user/src/components/layout/LoadingView.vue
+++ apps/Random-user/src/components/layout/LoadingView.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .text-center.bg-slate-100.rounded.h-full.set-item-center
-    a-spin(size="large")
+    Icon(icon="eos-icons:bubble-loading" width="50" height="50" class="text-cyan-500")
 </template>
 <script setup></script>
 <style scoped></style>
