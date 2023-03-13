60a0136644d268f488ddafc1e3d735f7d973c522
Ben.chen <ben.chen@keyxentic.com>
2023-03-13 22:01:55 +0800

    chore: 移除未使用套件`ant-design-vue` 、 `axios`

--- apps/Random-user/src/main.ts
+++ apps/Random-user/src/main.ts
@@ -6,8 +6,5 @@ import App from './App.vue';
 import { createPinia } from 'pinia';
 const pinia = createPinia();
 
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-
 import { Icon } from '@iconify/vue';
-createApp(App).use(Antd).use(router).use(pinia).component('Icon', Icon).mount('#app');
+createApp(App).use(router).use(pinia).component('Icon', Icon).mount('#app');
